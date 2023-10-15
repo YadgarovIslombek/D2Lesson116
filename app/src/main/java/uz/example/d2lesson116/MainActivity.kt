4464cf118b55e96fbcfe78f6d51f37f8efa8347b
IslombekYadgarov <islam67747@gmail.com>
2023-10-15 13:36:37 +0500

    Coroutines start

--- app/src/main/java/uz/example/d2lesson116/MainActivity.kt
+++ app/src/main/java/uz/example/d2lesson116/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import uz.example.d2lesson116.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
@@ -18,41 +22,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
-            binding.progress.isVisible = true
-            binding.buttonLoad.isEnabled = false
-            loadCity {
-                binding.tvLocation.text = it
-                loadTemp(it) {it1->
-                    binding.tvTemperature.text = it1.toString()
-                    binding.progress.isVisible = false
-                    binding.buttonLoad.isEnabled = true
-                }
-            }
-
-        }
-    }
-
-    private fun loadTemp(city: String, callback: (Int) -> Unit) {
-        thread {
-            Thread.sleep(5000) //ikinnchi patok
-            Handler(Looper.getMainLooper()).post{
-                Toast.makeText(this, "Loading temperature for city $city", Toast.LENGTH_SHORT).show()
-            }
-            Handler(Looper.getMainLooper()).post{
-            callback.invoke(17)
+            lifecycleScope.launch {
+                loadData()
             }
         }
     }
 
-    private fun loadCity(callback: (String) -> Unit) {
-        thread {  //bu yangi patok
-            Thread.sleep(5000)
-            Handler(Looper.getMainLooper()).post{
-            callback.invoke("Urgench")
+    private suspend fun loadData() {
+        Log.d("TAG", "Load started: $this")
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temp = loadTemp(city)
+        binding.tvTemperature.text = temp.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
+        Log.d("TAG", "Load finished: $this")
+    }
 
-            }
-        }
-
-
+    private suspend fun loadTemp(city: String): Int {
+        delay(5000)
+        Toast.makeText(this, "Loading temperature for city $city", Toast.LENGTH_SHORT)
+            .show()
+        return 17
+    }
+    private suspend fun loadCity(): String {
+        delay(5000)
+        return "Urgench"
     }
 }
